c56175a316224d0ae8e72db653976479ecb91cac
lishuo <lishuo@juze.pro>
2021-01-05 16:36:14 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # eapp-corp-quick-start-fe
+this is a ding-talk demo project from https://github.com/opendingtalk/eapp-corp-quick-start-fe.
+its back-end project: https://github.com/lishuo0710/eapp-corp-quick-start-Csharp
---
+++ page/index/index.js
@@ -0,0 +1,75 @@
+let app = getApp();
+
+
+//内网穿透工具介绍:
+// https://open-doc.dingtalk.com/microapp/debug/ucof2g
+//替换成开发者后台设置的安全域名
+let domain =  "https://e36ea2eca9a6.ngrok.io";
+let url = domain + '/home/login';
+
+Page({
+    data:{
+        corpId: '',
+        authCode:'',
+        userId:'',
+        userName:'',
+        hideList: true,
+    },
+    loginSystem() {
+        dd.getAuthCode({
+            success:(res)=>{
+                this.setData({
+                    authCode:res.authCode
+                })
+                //dd.alert({content: "step1"});
+                dd.httpRequest({
+                    url: url,
+                    method: 'POST',
+                    data: {
+                        authCode: res.authCode
+                    },
+                    dataType: 'json',
+                    success: (res) => {
+                        // dd.alert({content: "step2"});
+                        console.log('success----',res)
+                        let userId = res.data.data.userId;
+                        let userName = res.data.data.userName;
+                        this.setData({
+                            userId:userId,
+                            userName:userName,
+                            hideList:false
+                        })
+                    },
+                    fail: (res) => {
+                        console.log("httpRequestFail---",res)
+                       dd.alert({content: JSON.stringify(res)});
+                    },
+                    complete: (res) => {
+                        dd.hideLoading();
+                    }
+                    
+                });
+            },
+            fail: (err)=>{
+                // dd.alert({content: "step3"});
+                dd.alert({
+                    content: JSON.stringify(err)
+                })
+            }
+        })
+
+    },
+    onLoad(){
+
+        let _this = this;
+
+        this.setData({
+            corpId: app.globalData.corpId
+        })
+        
+        //dd.alert({content: "step1"});
+         
+        
+        
+    }
+})
